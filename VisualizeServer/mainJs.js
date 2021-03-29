38c197606e5b90de939311071f84426d86004795
kissinger <kchen0723@gmail.com>
2021-03-28 20:39:55 -0700

    remove VisualizeServer

--- VisualizeServer/mainJs.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-const app = express();
-var clientPath = '../VisualizeClient/dist/VisualizeClient';
-app.use(express.static(clientPath));
-app.listen(8885);
-console.log("server is runnning");
