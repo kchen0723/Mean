d7c53a873161e1530ffb2f3eb9c122429780aaec
kissinger <kchen0723@gmail.com>
2021-04-02 20:04:44 -0700

    change runexpress.js to es 2015 module instead of commonjs.

--- visualizeserver/src/runexpress.js
+++ visualizeserver/src/runexpress.js
@@ -1,6 +1,6 @@
-var express = require('express');
+import express from 'express';
 const app = express();
 var clientPath = '../VisualizeClient/dist/VisualizeClient';
 app.use(express.static(clientPath));
-app.listen(8885);
+app.listen(8889);
 console.log("server is runnning");
